52b061be620e32458e70b787c5d10326926fe221
blazeclan/theodorosgkisis <theodoros.gkisis@blazeclan.com>
2024-10-07 14:25:30 +0300

    NEW

--- src/index.js
+++ src/index.js
@@ -14,10 +14,36 @@ app.use(express.json());
 // Serving static files
 app.use(express.static(path.join(__dirname, './public')));
 
-io.on('connection', () => {
+//Message to serve to the Clients when they are log in to our Chat App
+const message = "Welcome to our Chat Application"
+
+//Serve the 'connection' from server side to client side
+io.on('connection', (socket) => {
+
     console.log("New WebSocket Connection");
+    //Emit server with client
+    socket.emit('Message',message)
+    //Listen from client, and display the message to the server side
+    //Here we are emit the message to all users exept from one that is connected to the Chat App
+    socket.broadcast.emit('Message',"A new User is log in")
+
+    socket.on("sendMessage",(message) =>{
+        console.log("The message from the client is: ",message)
+        //Serve the message to all the client that are connectin to our server right now
+        io.emit('AllConnectiorMessage',message)
+    })
+
+    socket.on('sendLocation',(location) =>{
+        io.emit('Message', `https${location.latitude},${location.longtitude}`)
+    })
+    socket.on('disconnect',()=>{
+        io.emit('Message','User is left')
+    })
+
+
 });
 
+//Listen to port 3000
 server.listen(port, () => {
     console.log("Server is listening on port: " + port);
 });
--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,1 +1,35 @@
 const socket = io();
+
+//Take the parameters from index.html
+const form = document.getElementById('messageForm');
+const input = document.getElementById('messageInput');
+const messagesDiv = document.getElementById('messages');
+
+//Log the Welcome message to the client console
+socket.on('Message', message,  () =>{
+    console.log(message)
+})
+
+
+form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    const message = input.value;
+    if (message.trim() !== '') {
+        socket.emit('sendMessage', message); // Emit the message to the server
+        input.value = ''; // Clear the input field after sending
+    }
+})
+
+socket.on('AllConnectiorMessage', (message) =>{
+    console.log(message)
+})
+
+
+document.getElementById('Geolocation').addEventListener('click' ,() =>{
+    navigator.geolocation.getCurrentPosition((possition) =>{
+        socket.emit('sendLocation',{
+            latitude: possition.coords.latitude ,
+            longtitude: possition.coords.longitude
+        })
+    })
+})
